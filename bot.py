7eb8ddc3603f7cd2cfbe0ba6bffe543754e95e02
Joshua Wu <Joshua.wu.0910@gmail.com>
2022-02-08 08:27:01 -0800

    Clear test code, add wordle link message

--- bot.py
+++ bot.py
@@ -12,20 +12,10 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
     print(
         f'{client.user} is connected to {guild.name}',
     )
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$google.it hello'):
-        await message.channel.send('Hello World!')
+    client.channels.get('the-after-hours').send('https://www.powerlanguage.co.uk/wordle/')
 
 client.run(TOKEN)
