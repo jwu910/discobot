24ae84fd36c3b2a2b4429a1cdad98d43a61dcaa7
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-01-08 11:57:48 -0800

    chore: rework bot for modals and slash commands

--- bot.py
+++ bot.py
@@ -3,21 +3,51 @@ import os
 import sys
 
 import discord
+from discord import app_commands, ui
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+TARGET_CHANNEL="1061716765740109834"
 
-@client.event
-async def on_ready():
-    print(
-        f'{client.user} is connected to {guild.name}',
-    )
+"""
+https://discordpy.readthedocs.io/en/latest/interactions/api.html?highlight=app+commands#modal
+"""
+class Client(discord.Client):
+    def __init__(self):
+        super().__init__(intents = discord.Intents.default())
+        self.synced = False
 
-    client.channels.get('the-after-hours').send('https://www.powerlanguage.co.uk/wordle/')
-    sys.exit(0)
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync(guild = discord.Object(id=GUILD))
+            self.synced = True
+        print(
+            f'{client.user} is connected to {self.user}',
+        )
+
+class TestModal(ui.Modal, title="Announcement"):
+    content = ui.TextInput(label="Content", style=discord.TextStyle.paragraph)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f'Thanks for your response, title is {self.title}!', ephemeral=True)
+
+client = Client()
+tree = app_commands.CommandTree(client)
+
+@tree.command(guild = discord.Object(id=GUILD), name="josh-test", description="Test description")
+async def slash123(interaction: discord.Interaction):
+    await interaction.response.send_message(f"This is working", ephemeral = True)
+
+@tree.command(guild = discord.Object(id=GUILD), name="modal", description="Test description")
+async def modalTest(interaction: discord.Interaction):
+    modal = TestModal()
+    await interaction.response.send_modal(modal)
+    # await interaction.response.send_message(modal)
+
+
 
 client.run(TOKEN)
